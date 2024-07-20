10714fc1747689750c715a33576b68f6b3852ffd
Aditya Sharma <aditya.sh54321@gmail.com>
2024-07-21 00:27:29 +0530

    commit

---
+++ javascript/skills.js
@@ -0,0 +1,57 @@
+const allSkills = 
+{
+    'Frontend Development':{
+        "REACT":30,
+        "CSS":55,
+        "TAILWIND CSS":80,
+    },
+    'Backend Development':{
+        "NODEJS":70,
+        "EXPRESSJS":60,
+        "POSTGRESQL":80
+    },
+    "Languages":{
+        "C/C++":50,
+        "JAVASCRIPT":70,
+        "TYPESCRIPT":85
+    },
+    'Other Skills':{
+        "PHOTOSHOP":50,
+        "ADOBE LIGHTROOM":75,
+        "CANVA":90
+    },
+}
+let skillCount = 1;
+function AddSkillModule(title,progressObj)
+{
+    function createElement(tag,className,parent)
+    {
+        const temp = document.createElement(tag);
+        temp.classList.add(className);
+        if(parent != undefined)
+        {
+            parent.appendChild(temp);
+        }
+        return temp;
+    }
+    
+    const moduleObj = createElement("div","card",skillsContainer);
+    moduleObj.classList.add("animate");
+    moduleObj.classList.add(`delay-${skillCount++}`);
+    moduleObj.classList.add(`shadow`);
+    createElement("div","card-title",moduleObj).innerHTML = title;
+    for(const key of Object.keys(progressObj))
+    {
+        const skillBar = createElement("div","skill-bar",moduleObj);
+        createElement("div","skill-bar-title",skillBar).innerHTML = key;
+        const fill = createElement("div","skill-bar-meter-fill",createElement("div","skill-bar-meter",skillBar));
+        fill.classList.add("repeat");
+        fill.style.width = `${progressObj[key]}%`;
+    }
+}
+
+const skillsContainer = document.querySelector("#skills-container");
+for(const key of Object.keys(allSkills))
+{
+    AddSkillModule(key,allSkills[key]);
+}
